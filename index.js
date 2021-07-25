eb4c61e7b57d39c89043a522682513565da9303e
Jindrich Samec <jindrich.samec@xjs.cz>
2021-07-25 09:46:35 +0200

    input validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Router = require("koa-router");
 const mysql = require("mysql2/promise");
 const bodyParser = require("koa-bodyparser");
 const config = require("./config");
+const { validate } = require("jsonschema");
+const readingSchema = require("./reading.schema.json");
 
 let connection = null;
 
@@ -39,21 +41,27 @@ async function insertConsumption(
     const connection = await getMysqlConnection();
     return connection.query(`INSERT INTO consumption SET ${values.join(", ")}`);
   }
-
 }
 
 const router = new Router({ prefix: "/api/1.0" });
 router.put("/reading", async (ctx, next) => {
   try {
     const { body } = ctx.request;
-    const {
-      gas = null,
-      electricity = null,
-      hotWater = null,
-      coldWater = null,
-    } = body;
-    await insertConsumption(gas, electricity, coldWater, hotWater);
-    ctx.response.status = 200;
+    const validationResult = validate(body, readingSchema)
+    if (validationResult.valid) {
+      const {
+        gas = null,
+        electricity = null,
+        hotWater = null,
+        coldWater = null,
+      } = body;
+      await insertConsumption(gas, electricity, coldWater, hotWater);
+      ctx.response.status = 200;
+    } else {
+      console.warn("Invalid input", validationResult.errors);
+      ctx.response.status = 400;
+      ctx.response.body = validationResult.errors;
+    }
   } catch (err) {
     console.error("Saving failed because of: ", err.message);
     ctx.response.status = 500;
