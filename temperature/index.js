ac9cc8475858dad8aea80f38361afad90ec042de
Jindrich Samec <jindrich.samec@xjs.cz>
2021-10-17 09:16:47 +0200

    MI tokens

--- temperature/index.js
+++ temperature/index.js
@@ -1,8 +1,14 @@
 const noble = require("@abandonware/noble");
 const { Parser, EventTypes, SERVICE_DATA_UUID } = require("./parser");
-const { insertTemperature } = require('./storage')
+const { insertTemperature } = require("./storage");
 
-const MI_TOKEN = "27535a40329effc1de539201b8280c15";
+// const MI_TOKEN = "27535a40329effc1de539201b8280c15";
+const TOKENS = [
+  "453e4ddc667863f2c8665d3e29ae7c8f",
+  "87f532e43b1523c4e461ffb650f03d5c",
+  "00adb26a0d30e6a9b3173962a8753bc9",
+  "b4ca84809a1bb1cf62419f3e5f614b0a"
+];
 
 noble.on("stateChange", async (state) => {
   if (state === "poweredOn") {
@@ -20,37 +26,63 @@ function getValidServiceData(serviceData) {
   );
 }
 
-const measurement = {
+
+const measurements = [];
+
+const findOrCreateMeasurement = (deviceMacAddress) => {
+  let measurement = measurements.find((m) => m.deviceMacAddress === deviceMacAddress)
+  if (!measurement) {
+    measurement = createMeasurement(deviceMacAddress)
+    measurements.push(measurement)
+  }
+  return measurement;
+}
+
+const createMeasurement = (deviceMacAddress) => {
+  return {
   temperature: null,
   humidity: null,
   battery: null,
-  deviceMacAddress: '-'
+  deviceMacAddress,
 };
+}
 
 noble.on("discover", async (peripheral) => {
-  const { advertisement: { serviceData } = {}, id, address } = peripheral;
+  const { advertisement: { serviceData, localName } = {}, id, address } = peripheral;
 
-  const miData = getValidServiceData(peripheral.advertisement.serviceData);
-  if (peripheral.advertisement.localName && miData) {
-    const result = new Parser(miData.data, MI_TOKEN).parse();
+  const miData = getValidServiceData(serviceData);
 
-    if (result == null) {
+  if (localName === 'LYWSD03MMC' && address && miData) {
+    const parsed = TOKENS.map((token) => {
+      try {
+        const result = new Parser(miData.data, token).parse();
+        return {result, token};
+      } catch(err) {
+        console.error(`Parsing with token "${token}" fails because "${err.message}. Address: ${address} Name: ${localName}"`)
+      }
+      return {result: null, token}
+    });
+
+    const {result, token} = parsed.find((p) => Boolean(p.result));
+
+    if (!result) {
+      console.log(`No result for device Address: ${address} Name: ${localName}"`)
       return;
     }
 
     if (!result.frameControl.hasEvent) {
       return;
     }
-
     const { eventType, event } = result;
-    measurement.deviceMacAddress = peripheral.address;
+    console.log(`ADDRESS: ${address} Token: ${token} Event:`, event);
+    const measurement = findOrCreateMeasurement(address);
     switch (eventType) {
       case EventTypes.temperature: {
-        measurement.temperature = event.temperature * 10;
+        measurement.temperature = event.temperature * 10;
         break;
       }
       case EventTypes.humidity: {
-        measurement.humidity = event.humidity * 10;
+        measurement.humidity = event.humidity * 10;
         break;
       }
       case EventTypes.battery: {
@@ -58,8 +90,8 @@ noble.on("discover", async (peripheral) => {
         break;
       }
       case EventTypes.temperatureAndHumidity: {
-        measurement.temperature = event.temperature;
-        measurement.humidity = event.humidity;
+        measurement.temperature = event.temperature * 10;
+        measurement.humidity = event.humidity * 10;
         break;
       }
     }
@@ -67,8 +99,16 @@ noble.on("discover", async (peripheral) => {
 });
 
 const intervalId = setInterval(() => {
-  insertTemperature(measurement.temperature, measurement.humidity, measurement.battery, measurement.deviceMacAddress);
-  console.dir(measurement)
+  measurements.forEach((measurement) => {
+    insertTemperature(
+      measurement.temperature,
+      measurement.humidity,
+      measurement.battery,
+      measurement.deviceMacAddress
+    );
+    console.dir(measurement);
+  })
+
 }, 5 * 60 * 1000);
 
 // A4:C1:38:22:DF:73
